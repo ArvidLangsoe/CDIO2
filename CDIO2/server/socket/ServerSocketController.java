96071646f704364c82a43d2425cdd4573dd6ff2e
Arvid Langsø <arvid.k.langsoe@live.dk>
2017-03-02 15:54:49 +0100

    Added getLine() and getLine() commands

---
+++ CDIO2/server/socket/ServerSocketController.java
@@ -0,0 +1,47 @@
+package socket;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class ServerSocketController implements IServerSocketController {
+
+	Socket socket;
+	DataOutputStream outToWeight;
+	BufferedReader inFromWeight;
+	
+	public ServerSocketController(String ip, int port) throws UnknownHostException, IOException{
+			socket=new Socket(ip,port);	
+			outToWeight=new DataOutputStream(socket.getOutputStream());
+			inFromWeight=new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			
+	}
+	
+	@Override
+	public void sendCommand(String command) {
+		try {
+			outToWeight.writeBytes(command+"\n");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public String getLine() {
+		String weightMessage=null;
+		try {
+			weightMessage =inFromWeight.readLine();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return weightMessage;
+	}
+
+}
