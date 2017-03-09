ef0bb2fce74ae4e648e7996f6e2bc3ab19cc7214
Jeppe <Jeppe@DESKTOP-HUK674P.eduroam.wireless.dtu.dk>
2017-03-09 15:28:14 +0100

    Created methods for commands

--- CDIO2/server/controller/ServerMainController.java
+++ CDIO2/server/controller/ServerMainController.java
@@ -45,12 +45,8 @@ public class ServerMainController {
 		try {
 			socket.sendCommand("K 3");
 			checkAnswer("K A");
-			socket.sendCommand("RM20 8 \"Enter UserID\" \"\" \"&3\"");
-			checkAnswer("RM20 B");
-			checkAnswer("RM20 A");
-			socket.sendCommand("P111 \"Username\"");
-			checkAnswer("P111 A");
-			checkAnswer("K F 4");
+			RM20Command("Enter UserID");
+			P111Command("Username");
 		} catch (WrongAnswerException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -60,47 +56,75 @@ public class ServerMainController {
 
 	public void measureWeight()
 	{
+		double tara = 0;
+		double netto = 0;
+		double brutto = 0;
+		double actualBrutto = 0;
+		final double TOLERANCE = 0.1;
+
 		while(true) {
 			try {
-				socket.sendCommand("RM20 8 \"Enter Batch Number\" \"\" \"&3\"");
-				checkAnswer("RM20 B");
-				checkAnswer("RM20 A");
-				socket.sendCommand("P111 \"ProductName\"");
-				checkAnswer("P111 A");
-				checkAnswer("K F 4");
-				socket.sendCommand("P111 \"Clear the weight\"");
-				checkAnswer("P111 A");
-				checkAnswer("K F 4");
+				RM20Command("Enter Batch Number");
+				P111Command("Product Name");
+				P111Command("Clear the weight");
 				socket.sendCommand("T");
 				checkAnswer("T S");
-				socket.sendCommand("P111 \"Put Tara on the weight\"");
-				checkAnswer("P111 A");
-				checkAnswer("K F 4");
-				socket.sendCommand("S");
-				checkAnswer("S S");
-				socket.sendCommand("T");
-				checkAnswer("T S");
-				socket.sendCommand("P111 \"Put the Product one the Tara\"");
-				checkAnswer("P111 A");
-				checkAnswer("K F 4");
-				socket.sendCommand("S");
-				checkAnswer("S S");
-				socket.sendCommand("T");
-				checkAnswer("T S");
-				socket.sendCommand("P111 \"Clear The weight\"");
-				checkAnswer("P111 A");
-				checkAnswer("K F 4");
-				socket.sendCommand("S");
-				checkAnswer("S S");
-				socket.sendCommand("P111 \"Measurement OK\"");
-				checkAnswer("P111 A");
-				checkAnswer("K F 4");
+
+				tara = getMeasurement("Put Tara on the weight");
+				netto = getMeasurement("Put the Product one the Tara");
+				brutto = tara + netto;
+
+				actualBrutto = getMeasurement("Clear The weight");
+				if(TOLERANCE > Math.abs(brutto - actualBrutto)) {
+					socket.sendCommand("P111 \"Measurement OK\"");
+					checkAnswer("P111 A");
+					checkAnswer("K F 4");
+				}
+				else {
+					socket.sendCommand("P111 \"Measurement OK\"");
+				}
+
 			} catch (WrongAnswerException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			
+
 		}
+	}
+
+	public String RM20Command(String message) throws WrongAnswerException {
+		String out = "";
+		socket.sendCommand("RM20 8 \"" + message + "\" \"\" \"&3\"");
+		checkAnswer("RM20 B");
+		out = checkAnswer("RM20 A");
+
+
+		return out;
+	}
+
+	public void P111Command(String message) throws WrongAnswerException {
+		socket.sendCommand("P111 \"" + message + "\"");
+		checkAnswer("P111 A");
+		checkAnswer("K F 4");
+
+	}
+
+	public double getMeasurement(String message) {
+		double measurement = -999999;
+		try {
+			socket.sendCommand("P111 \"" + message + "\"");
+			checkAnswer("P111 A");
+			checkAnswer("K F 4");
+			socket.sendCommand("S");
+			measurement = Double.parseDouble(checkAnswer("S S"));
+			socket.sendCommand("T");
+			checkAnswer("T S");
+		} catch (WrongAnswerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return measurement;
 	}
 
 	public String checkAnswer(String expectedAnswer) throws WrongAnswerException{
