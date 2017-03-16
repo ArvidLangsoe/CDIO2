b62f75b8c962bd2cca5c97cc6c978e5503272e72
Mikkel Holmbo Lund <math-lund@outlook.dk>
2017-03-16 14:09:29 +0100

    Gone.

--- CDIO2/server/main/Main.java
+++ CDIO2/server/main/Main.java
@@ -6,8 +6,7 @@ public class Main {
 	
 	public static void main(String[] args) {
 		ServerMainController controller = new ServerMainController();
-		//String ip = "169.254.2.2";
-		String ip = "10.16.139.112";
+		String ip = "169.254.2.2";
 		controller.startConnection(ip, true);
 		controller.run();
 	}
